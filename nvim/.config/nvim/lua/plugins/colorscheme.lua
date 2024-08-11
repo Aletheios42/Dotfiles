d2b593ce65873e8d95617acbcd228d852d8b85a9
Aletheios42 <alejandropintosdev@gmail.com>
2024-08-11 06:06:31 +0200

    polybar hecha y fuentes añadidas

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,31 +1,43 @@
--- Este plugin es pura estetica
+-- -- Este plugin es pura estetica
 return {
-  {
-    "scottmckendry/cyberdream.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = function(_, opts)
-      opts.transparent = true
-      opts.italic_comments = true
-    end,
-  },
+  -- add gruvbox
+  { "wittyjudge/gruvbox-material.nvim" },
+
+  -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "cyberdream",
+      colorscheme = "gruvbox-material",
     },
   },
-
-  -- modicator (auto color line number based on vim mode)
-  {
-    "mawkler/modicator.nvim",
-    dependencies = "scottmckendry/cyberdream.nvim",
-    init = function()
-      -- These are required for Modicator to work
-      vim.o.cursorline = false
-      vim.o.number = true
-      vim.o.termguicolors = true
-    end,
-    opts = {},
-  },
 }
+-- return {
+--   {
+--     "scottmckendry/cyberdream.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     opts = function(_, opts)
+--       opts.transparent = true
+--       opts.italic_comments = true
+--     end,
+--   },
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "cyberdream",
+--     },
+--   },
+--
+--   -- modicator (auto color line number based on vim mode)
+--   {
+--     "mawkler/modicator.nvim",
+--     dependencies = "scottmckendry/cyberdream.nvim",
+--     init = function()
+--       -- These are required for Modicator to work
+--       vim.o.cursorline = false
+--       vim.o.number = true
+--       vim.o.termguicolors = true
+--     end,
+--     opts = {},
+--   },
+-- }
