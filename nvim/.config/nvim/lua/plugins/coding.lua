3dc8b3478339d320e55e9a5f06104f5b67c54d27
Aletheios42 <alejandropintosdev@gmail.com>
2024-08-16 22:56:52 +0200

    lista de comandos

--- nvim/.config/nvim/lua/plugins/coding.lua
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -1,4 +1,4 @@
--- Este plugin te permite renombrar archivos, lo tengo mapeado:
+-- Este plugin te permite renombrar archivos, y variables lo tengo mapeado:
 --  vim.api.nvim_set_keymap('n', '<leader>rn', ':IncRename ', { noremap = true, silent = true })
 return {
   "smjonas/inc-rename.nvim",
