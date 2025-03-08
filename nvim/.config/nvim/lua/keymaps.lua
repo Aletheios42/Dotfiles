8ba0a5d6a24dd9ccd6eadc90a0d9eb1cad87d80c
Aletheios42 <alejandropintosdev@gmail.com>
2025-03-08 13:29:58 +0100

    @resurrection no carga el contenido de los paneles de nvim con el reboot

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -5,8 +5,8 @@ local opts = { noremap = true, silent = true }
 keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Split windows
-keymap.set('n', 'ss', ':vsplit<Return>', opts)
-keymap.set('n', 'sv', ':split<Return>', opts)
+keymap.set('n', 's|', ':vsplit<Return>', opts)
+keymap.set('n', 's-', ':split<Return>', opts)
 
 -- Tabs
 keymap.set('n', 'te', ':tabedit', opts)
--- nvim/.config/nvim/lua/plugins/ui/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/ui/colorscheme.lua
@@ -1,15 +1,29 @@
 return {
+  -- add gruvbox
+  { 'wittyjudge/gruvbox-material.nvim' },
+
+  -- Configure LazyVim to load gruvbox
   {
-    'scottmckendry/cyberdream.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require('cyberdream').setup {
-        transparent = true,
-        italic_comments = true,
-        borderless_telescope = false,
-      }
-      vim.cmd [[colorscheme cyberdream]]
-    end,
+    'LazyVim/LazyVim',
+    opts = {
+      colorscheme = 'gruvbox-material',
+    },
   },
 }
+
+-- Color azul negro como el de la terminal, no me gusta que sea tan parecido
+-- return {
+--   {
+--     'scottmckendry/cyberdream.nvim',
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--       require('cyberdream').setup {
+--         transparent = true,
+--         italic_comments = true,
+--         borderless_telescope = false,
+--       }
+--       vim.cmd [[colorscheme cyberdream]]
+--     end,
+--   },
+-- }
